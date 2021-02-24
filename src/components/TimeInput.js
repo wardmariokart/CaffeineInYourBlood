dcaffa78054fb180371f3e68b3d775705a432109
ward <ward.van.bever@student.howest.be>
2021-02-24 15:20:51 +0100

    TimeInput.js -> time loops around. only works when using the arrows!!

--- src/components/TimeInput.js
+++ src/components/TimeInput.js
@@ -1,37 +1,60 @@
+import {modulo} from './../helpers.js';
+
 export const TimeInput = ({time, setTime}) => {
 
+
+    /* NOT DONE YET.... */
     const handleTimeChange = (updatedProperties) =>
     {
         const timeKeys = Object.keys(time);
-        console.log(timeKeys);
         
-        const timeClone = {...time};
+        let timeClone = {...time};
         timeKeys.forEach(key => {
             if(key in updatedProperties)
             {
-                timeClone[key] = updatedProperties[key];
+                const maxDigits = 2;
+                let newValue = updatedProperties[key];
+                if(newValue.toString.length > maxDigits) newValue = newValue.toString().slice(-maxDigits);
+                timeClone[key] = newValue;
             }
-        })
+        });
 
+        console.log(`${timeClone.hours}h ${timeClone.minutes}`);
+
+
+        timeClone = toValidTime(timeClone);
         setTime(timeClone);
     }
 
-    const handleChangeMinutes = (newMinutes) => {
+    const toValidTime = (toValidate) =>
+    {
+        const minutes = toValidate.minutes ?? time.minutes;
+        const hours = toValidate.hours ?? time.hours;
+
+        const hoursInDay = 24;
         const minutesInHour = 60;
-        const totalMinutes = time.hours * minutesInHour + newMinutes;
-        const convertedHours = Math.floor(totalMinutes/minutesInHour);
-        const convertedMinutes = totalMinutes - convertedHours * minutesInHour;
-        handleTimeChange({hours: convertedHours, minutes: convertedMinutes});
+
+        const totalMinutes = modulo(hours * minutesInHour + minutes, hoursInDay * minutesInHour);
+        const validHours = modulo(Math.floor(totalMinutes/minutesInHour), hoursInDay);
+        const validMinutes = totalMinutes - validHours * minutesInHour;
+        console.log({validHours, validMinutes});
+        return {hours: validHours, minutes: validMinutes};
+    };
+
+    const handleValueChange = (e) => {
+        // empty => 0
+        // 01 => 1
+        // 123 => 12
+
+
+
+
     }
 
-    const handleChangeHours = (newHours) => {
-
-        /* handleTimeChange({hours: newHours}) */
-    }
 
     return <span>
-        <input type='number' value={time.hours} onChange={(e) => handleChangeHours(parseInt(e.currentTarget.value))}></input>
+        <input type='number' maxLength={2} value={time.hours} onChange={(e) => handleTimeChange({hours: parseInt(e.currentTarget.value)})}></input>
         :
-        <input type='number' value={time.minutes} onChange={(e) => handleChangeMinutes(parseInt(e.currentTarget.value))}></input>
+        <input type='number' maxLength={2} value={time.minutes} step={15} onChange={(e) => handleTimeChange({minutes: parseInt(e.currentTarget.value)})}></input>
     </span>;
 }
--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,5 @@
 export const bDebugLog = true;
+
+export const modulo = (number, modulo) => ((number % modulo) + modulo) % modulo // works as expected for negative values. {-1 % 24  = -1} but {modulo(-1, 24) = 23}
+
+export const leadingZeros = (integer, digits) => {let out = integer.toString; while(out.length < digits) out = '0' + out; return out;}
